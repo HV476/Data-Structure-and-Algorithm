2da5901e2d2da25efffa00416a6873c47c22d578
Harsh Vardhan <183703833+HV476@users.noreply.github.com>
2026-03-06 11:31:49 +0530

    Time: 18 ms (21.92%), Space: 47.1 MB (94.97%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,49 +1,45 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-
         int m = grid.length;
         int n = grid[0].length;
-
+        int dist = -1;
         Queue<int[]> q = new LinkedList<>();
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1){
-                    q.offer(new int[]{i, j});
+                    q.add(new int[]{i , j});
                 }
             }
-        }
-
-        if(q.isEmpty() || q.size() == m * n){
+        }if(q.isEmpty() || q.size() == m*n){
             return -1;
         }
-
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1};
-
-        int distance = -1;
+        return bfs(q , grid , dist);
+    }
+    public int bfs(Queue<int[]> q , int[][] grid , int dist){
+        int[] dr = {-1 , 0 , 1 , 0};
+        int[] dc = {0 , 1, 0 , -1};
 
         while(!q.isEmpty()){
             int size = q.size();
-            distance++;
+            dist++;
 
             for(int i = 0; i < size; i++){
                 int[] curr = q.poll();
                 int r = curr[0];
                 int c = curr[1];
 
-                for(int d = 0; d < 4; d++){
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
+                for(int j = 0; j < 4; j++){
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
 
-                    if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == 0){
+                    if(nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 0){
                         grid[nr][nc] = 1;
-                        q.offer(new int[]{nr, nc});
+                        q.add(new int[]{nr , nc});
                     }
                 }
             }
         }
-
-        return distance;
+        return dist;
     }
 }
