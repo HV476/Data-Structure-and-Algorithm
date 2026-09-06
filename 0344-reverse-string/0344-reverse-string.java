8e68f829cf1148d092a58504b0c68d7993908334
Harsh Vardhan <hv47683@gmail.com>
2026-09-06 06:10:26 +0530

    Time: 0 ms (100%), Space: 48.1 MB (87.99%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        if(left >= right){
+            return;
+        }
+        
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
